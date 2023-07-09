5b4f531c46d16cdf17a619a521dc41a904e885a1
ttasjwi <ttasjwi920@gmail.com>
2023-07-09 23:35:18 +0900

    커스텀 예외 정의

---
+++ src/main/java/com/ttasjwi/securitypractice/config/exception/CustomException.java
@@ -0,0 +1,19 @@
+package com.ttasjwi.securitypractice.config.exception;
+
+public abstract class CustomException extends RuntimeException {
+
+    public CustomException() {
+    }
+
+    public CustomException(String message) {
+        super(message);
+    }
+
+    public CustomException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public CustomException(Throwable cause) {
+        super(cause);
+    }
+}
